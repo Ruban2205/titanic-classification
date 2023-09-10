9a172f019e6991e3c11d5ba5d0ebce596f61e733
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-10 21:33:22 +0530

    ADD strings to the OUTPUT

---
+++ streamlitapi.py
@@ -0,0 +1,48 @@
+import pickle
+import streamlit as st  
+import numpy as np 
+
+model_file = pickle.load(open('notebook/model.pkl','rb'))
+
+def pred_output(user_input): 
+    model_input = np.array(user_input)
+    ypred = model_file.predict(model_input.reshape(-1,7))
+    return ypred[0]
+
+
+def main(): 
+    st.title("Titanic Classification - rubangino.in")
+
+    # Input Variables 
+    passenger_class = st.text_input("Enter the passenger class: ")
+    sex = st.text_input("Enter your sex (Male/Female): ")
+
+    # if sex == "Male" or sex == "female": 
+    #     sex = 0
+    # elif sex == "Female" or sex == "female": 
+    #     sex = 1
+
+    age = st.text_input("Enter your age: ")
+    sibsp = st.text_input("Enter your Siblings: ")
+    parch = st.text_input("Enter your parch: ")
+    fare = st.text_input("Enter your Fare: ")
+    embarked = st.text_input("Enter your Embarked: ")
+
+    # Button to predict
+    if st.button('Predict'): 
+        user_input = [passenger_class, sex, age, sibsp, parch, fare, embarked]
+        make_prediction = pred_output(user_input)  
+
+        if make_prediction == 0: 
+            make_prediction = "Not Survived :("
+        elif make_prediction == 1: 
+            make_prediction = "Survived :)"
+
+        st.success(make_prediction)
+
+if __name__ == '__main__': 
+    main()
+
+
+# SEX - Male=0 Female=1
+# Embarked - C=Cherbourg Q=Queentown S=Southampton
